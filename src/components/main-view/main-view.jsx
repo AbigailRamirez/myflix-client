3e094ca88dd906afe2cd18725ec2df6baae08806
AbigailRamirez <ramirezabigail01@gmail.com>
2023-03-30 17:06:35 -0400

    fixed syntax

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -11,57 +11,52 @@ export const MainView = () => {
     const [user, setUser] = useState(null);
   
     useEffect(() => {
-        fetch("https://filmeo-app.herokuapp.com/movies")
+      fetch("https://filmeo-app.herokuapp.com/movies")
         .then((response) => response.json())
         .then((data) => {
-            const moviesFromApi = data.map((movie) => {
-                //will update eventually with actors and genre
-                return {
-                    id: movie._id,
-                    title: movie.Title,
-                    image: movie.ImagePath,
-                    director: movie.Director?.Name,
-                    description: movie.Description
-                };
-            });
-            setMovies(moviesFromApi);
+          const moviesFromApi = data.map((movie) => {
+            // will update eventually with actors and genre
+            return {
+              id: movie._id,
+              title: movie.Title,
+              image: movie.ImagePath,
+              director: movie.Director?.Name,
+              description: movie.Description,
+            };
+          });
+          setMovies(moviesFromApi);
         });
     }, []);
-
+  
     if (!user) {
-        return <LoginView onLoggedIn={(user) => setUser(user)} />;
+      return <LoginView onLoggedIn={(user) => setUser(user)} />;
     }
-
+  
     if (selectedMovie) {
-        return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-        );
+      return (
+        <MovieView
+          movie={selectedMovie}
+          onBackClick={() => setSelectedMovie(null)}
+        />
+      );
     }
-  ]);
-
-  const [selectedMovie, setSelectedMovie] = useState(null);
-
-  if (selectedMovie) {
+  
+    if (movies.length === 0) {
+      return <div>The list is empty!</div>;
+    }
+  
     return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-    );
-  }
-
-  if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-  }
-
-  return (
-    <div>
+      <div>
         {movies.map((movie) => (
-            <MovieCard 
-                key={movie.id} 
-                movie={movie} 
-                onMovieClick={(newSelectedMovie) => {
-                    setSelectedMovie(newSelectedMovie);
-                  }}
-            />
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onMovieClick={(newSelectedMovie) => {
+              setSelectedMovie(newSelectedMovie);
+            }}
+          />
         ))}
-    </div>
-  );
-};
+      </div>
+    );
+  };
+  
